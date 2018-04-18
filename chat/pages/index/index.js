225961139c2ae996b038b6109f02138b3e97e177
syc <doingself@163.com>
2018-04-18 15:28:37 +0800

    mqtt websocket request

--- chat/pages/index/index.js
+++ chat/pages/index/index.js
@@ -5,84 +5,114 @@ const app = getApp()
 const Paho = require('../../utils/paho-mqtt.js')
 
 let client = null;
+// let serip = "baidu.com";
+// let serip = "10.10.16.69";
+// let serport = 1883;
+let serip = "test.haha.com";
+let serport = 8080;
 
 Page({
-    data: {
-        motto: 'Hello World',
-        userInfo: {},
-        hasUserInfo: false,
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
-    },
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
 
-    init: function() {
-        /**
-         * IoT 初始化
-         */
-        console.log("+++++++++ init 1")
-        client = new Paho.Client('10.10.16.61', 1883, "clientId");
-        console.log("+++++++++ init 2")
+  onLoad: function () {
+    console.log("+++++++++ onLoad");
+  },
 
-        client.connect({
-            userName: "test",
-            password: "test",
-            useSSL: true,
-            keepAliveInterval: 60,
-            onSuccess: function() {
-                console.log("+++++++++ init 3")
-                client.subscribe("okf/msg/#", {
-                    qos: 1
-                });
-            }
+  // MARK: MQTT
+  connectMqtt: function () {
+
+    /**
+     * IoT 初始化
+     */
+    console.log("+++++++++ connectMqtt 1-6 start")
+    console.log("+++++++++ connectMqtt 1")
+    client = new Paho.Client(serip, serport, "clientId");
+    console.log("+++++++++ connectMqtt 2")
+
+    client.connect({
+      userName: "test",
+      password: "test",
+      useSSL: true,
+      keepAliveInterval: 60,
+      onSuccess: function () {
+        console.log("+++++++++ connectMqtt 3")
+        client.subscribe("okf/msg/#", {
+          qos: 1
         });
+      }
+    });
 
-        console.log("+++++++++ init 4")
-    },
-    msg_callback: function() {
-        console.log("+++++++++ msg_callback 1")
-        let that = this;
-        /**
-         * IoT 消息回调
-         */
-        client.onMessageArrived = function(msg) {
-            console.log("+++++++++ msg_callback 2")
-            console.log(msg.topic, msg.payloadString);
-            that.setData({
-                msg: msg.payloadString,
-            })
-            console.log("+++++++++ msg_callback 3")
-        }
-    },
-    publish_msg: function(topic, msg) {
-        /**
-         * IoT 发送消息
-         */
-        var message = new Paho.Message(msg);
-        message.destinationName = topic;
-        message.qos = 1;
-        client.send(message);
-    },
-    formSubmit: function(e) {
-        let msg = e.detail.value.msg;
-        this.publish_msg("okf/msg/test", msg);
-    },
-    onLoad: function() {
-        console.log("+++++++++ onLoad 1")
+    console.log("+++++++++ connectMqtt 4")
 
-        this.init();
-        this.msg_callback();
-        console.log("+++++++++ onLoad 2")
-
-    },
-    //事件处理函数
-    bindViewTap: function(e) {
-        console.log("+++++++++ bindViewTap")
-        console.log(e)
-        this.publish_msg("okf/msg/test", "hahaha bindViewTap");
-    },
-    getUserInfo: function(e) {
-        console.log("+++++++++ getUserInfo")
-        console.log(e)
-
-        this.publish_msg("okf/msg/test", "hahaha getUserInfo");
+    let that = this;
+    /**
+     * IoT 消息回调
+     */
+    client.onMessageArrived = function (msg) {
+      console.log("+++++++++ connectMqtt 5")
+      console.log(msg.topic, msg.payloadString);
+      that.setData({
+        msg: msg.payloadString,
+      })
+      console.log("+++++++++ connectMqtt 6")
     }
+    console.log("+++++++++ connectMqtt 1-6 end")
+  },
+  publishMqttMsg: function () {
+    /**
+     * IoT 发送消息
+     */
+    let topic = "haha/topicTest"
+    let msg = "hello mqtt by publish mqtt msg"
+
+    var message = new Paho.Message(msg);
+    message.destinationName = topic;
+    message.qos = 1;
+    client.send(message);
+  },
+
+  // MARK: WebSocket
+  connectWebsocket: function () {
+    console.log("++++++++ connectWebsocket")
+    wx.connectSocket({
+      url: 'wss://test.haha.com:433/mqtt',
+      data: {
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "GET"
+    })
+    wx.onSocketOpen(function (res) {
+      console.log('WebSocket连接已打开！')
+      console.log(res)
+    })
+    wx.onSocketError(function (res) {
+      console.log('WebSocket连接打开失败，请检查！')
+      console.log(res)
+    })
+    wx.onSocketMessage(function (res) {
+      console.log('WebSocket收到服务器内容：')
+      console.log(res)
+    })
+  },
+
+  // MARK: Request
+  testrequest: function (e) {
+    console.log("++++++++ test request")
+    wx.request({
+      url: 'https://test.haha.com:8080/mqtt',
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      success: function (res) {
+        // success
+        console.log(res)
+      }
+    })
+  }
 })
